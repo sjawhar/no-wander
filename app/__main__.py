fac45c90e9f50e26431ee62682df52039c5742ab
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-10-27 19:50:40 -0500

    ACC, GYRO, and PPG data sources

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,15 @@
 import argparse
 import logging
 from .constants import PACKAGE_NAME
-from .record import get_duration, run_session, start_stream
+from .record import (
+    get_duration,
+    run_session,
+    start_stream,
+    SOURCE_ACC,
+    SOURCE_EEG,
+    SOURCE_GYRO,
+    SOURCE_PPG,
+)
 from .viewer import visualize
 from pathlib import Path
 from time import gmtime, strftime
@@ -15,30 +23,58 @@ def main(args):
     duration = get_duration(args.duration)
     filepath = (Path(__file__).parent / args.filename).resolve()
 
-    stream_manager = start_stream(args.address)
+    stream_manager = start_stream(args.address, args.sources)
     if not stream_manager:
         exit(1)
     if not args.skip_visualize:
         visualize()
 
-    run_session(duration, filepath, stream_manager)
+    run_session(duration, args.sources, filepath, stream_manager)
     stream_manager(False)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Capture meditation session')
     parser.add_argument(
+        '-a', '--address',
+        help='Skip search and stream from specified address',
+    )
+    parser.add_argument(
+        '-c', '--acc',
+        dest='sources',
+        action='append_const',
+        const=SOURCE_ACC,
+        help='Record accelerometer measurements',
+    )
+    parser.add_argument(
         '-d', '--duration',
         type=int,
         help='Length of the meditation session in minutes',
+    )
+    parser.add_argument(
+        '--no-eeg',
+        dest='eeg',
+        action='store_false',
+        default=True,
+        help='Record gyroscope measurements',
     )
     parser.add_argument(
         '-f', '--filename',
         help='Filename where recording should be saved',
     )
     parser.add_argument(
-        '-a', '--address',
-        help='Skip search and stream from specified address',
+        '-g', '--gyro',
+        dest='sources',
+        action='append_const',
+        const=SOURCE_GYRO,
+        help='Record gyroscope measurements',
+    )
+    parser.add_argument(
+        '-p', '--ppg',
+        dest='sources',
+        action='append_const',
+        const=SOURCE_PPG,
+        help='Record PPG measurements',
     )
     parser.add_argument(
         '-s', '--skip-visualize',
@@ -47,16 +83,16 @@ if __name__ == '__main__':
         help='Skip visualization and stability check',
     )
     parser.add_argument(
-        '-v', '--verbose',
-        action='store_true',
-        default=False,
-        help='Enable verbose logging',
-    )
-    parser.add_argument(
         '-t', '--test',
         action='store_true',
         default=False,
         help='Store data in test directory',
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        default=False,
+        help='Enable verbose logging',
     )
 
     args = parser.parse_args()
@@ -66,6 +102,9 @@ if __name__ == '__main__':
     elif not args.filename:
         data_dir = "test" if args.test else "prod"
         args.filename = f'data/{data_dir}/{strftime("%Y-%m-%d_%H-%M-%S", gmtime())}.csv'
+
+    if args.eeg:
+        args.sources.append(SOURCE_EEG)
 
     logger.debug(f'Received args {args}')
     main(args)
--- app/record.py
+++ app/record.py
@@ -13,12 +13,16 @@ RECORD_KEYS_QUIT = ['esc', 'q']
 RECORD_VALUE_END = 2
 RECORD_VALUE_RESPONSE = 1
 SOUND_BELL = (Path(__file__).parent / 'assets' / 'bell.wav').resolve()
+SOURCE_ACC = 'ACC'
+SOURCE_EEG = 'EEG'
+SOURCE_GYRO = 'GYRO'
+SOURCE_PPG = 'PPG'
 STREAM_ATTEMPTS_MAX = 5
 
 logger = logging.getLogger(PACKAGE_NAME + '.' + __name__)
 
 
-def start_stream(address, confirm=True):
+def start_stream(address, sources, confirm=True):
     if address is None:
         logger.info('No address provided, searching for muses...')
         muses = list_muses()
@@ -28,6 +32,12 @@ def start_stream(address, confirm=True):
         address = muses[0]['address']
         logger.debug(f'Found muse with address {address}.')
 
+    kwargs = {
+        'acc_enabled': SOURCE_ACC in sources,
+        'eeg_disabled': SOURCE_EEG not in sources,
+        'gyro_enabled': SOURCE_GYRO in sources,
+        'ppg_enabled': SOURCE_PPG in sources,
+    }
     keep_trying = True
     while keep_trying:
         logger.info(f'Establishing stream to {address}...')
@@ -35,10 +45,7 @@ def start_stream(address, confirm=True):
         while attempt < STREAM_ATTEMPTS_MAX:
             attempt += 1
             logger.info(f'Beginning stream attempt {attempt}...')
-            # TODO: Option to include PPG
-            # TODO: Option to include ACC
-            # TODO: Option to include GYRO
-            stream_process = Process(target=stream, args=(address,))
+            stream_process = Process(target=stream, args=(address,), kwargs=kwargs)
             stream_process.daemon = True
             stream_process.start()
             stream_process.join(7)
@@ -49,7 +56,7 @@ def start_stream(address, confirm=True):
             def stream_manager(restart=True):
                 logger.debug(f'Stream process alive: {stream_process.is_alive()}')
                 stream_process.terminate()
-                return start_stream(address, False) if restart else False
+                return start_stream(address, sources, False) if restart else False
 
             logger.info('Stream established!')
             return stream_manager
@@ -113,36 +120,48 @@ def play_bell():
         logger.error('Could not play bell sound')
 
 
-def record_chunks(duration, filepath, outlet, stream_manager):
+def record_chunks(duration, sources, filepath, stream_manager, outlet):
     filename_parts = filepath.name.split('.')
-    filename_parts.insert(1, '')
+    filename_parts = filename_parts[:-1] + [''] * 2 + filename_parts[-1:]
     start_time = time()
     get_remaining = lambda : duration + start_time - time()
 
-    i = 0
+    chunk_num = 1
     remaining = duration
     while remaining > RECORD_CHUNK_DURATION_MIN:
         chunk_duration = min(remaining, RECORD_CHUNK_DURATION_MAX)
-        filename_parts[1] = str(i)
-        chunk_filename = PurePath(filepath.parent) / '.'.join(filename_parts)
-        record_process = Process(target=record, args=(chunk_duration, str(chunk_filename)))
-
-        logger.info(f'Starting chunk {i+1} at {time()} for {chunk_duration} seconds')
-        record_process.start()
+        filename_parts[-2] = str(chunk_num)
+        record_processes = []
+        logger.info(f'Starting chunk {chunk_num} at {time()} for {chunk_duration} seconds')
+        for source in sources:
+            logger.debug(f'Starting {source} recording process...')
+            filename_parts[-3] = source
+            process = Process(
+                target=record,
+                args=(chunk_duration,),
+                kwargs={
+                    'filename': str(PurePath(filepath.parent) / '.'.join(filename_parts)),
+                    'dejitter': True,
+                    'data_source': source,
+                }
+            )
+            process.start()
+            record_processes.push(process)
 
         # # TODO: Push dummy sample to preserve recording if no responses
-        # record_process.join(5)
+        # record_processes[0].join(5)
         # dummy_timestamp = time()
         # logger.debug(f'Pushing dummy sample at {dummy_timestamp}')
         # outlet.push_sample([RECORD_VALUE_END], dummy_timestamp)
 
-        record_process.join(chunk_duration + 10)
-        if record_process.is_alive():
-            logger.warning('Recording process has idled. Terminating...')
-            record_process.terminate()
+        record_processes[0].join(chunk_duration + 10)
+        for si in range(len(sources)):
+            if record_processes[si].is_alive():
+                logger.warning(f'{sources[si]} recording process has idled. Terminating...')
+                record_processes[si].terminate()
 
         chunk_time = remaining - get_remaining()
-        logger.debug(f'Chunk {i+1} took {chunk_time} seconds')
+        logger.debug(f'Chunk {chunk_num} took {chunk_time} seconds')
         if chunk_time < chunk_duration:
             logger.warning('Error in stream. Restarting...')
             stream_manager = stream_manager()
@@ -150,10 +169,10 @@ def record_chunks(duration, filepath, outlet, stream_manager):
 
         remaining = get_remaining()
         logger.debug('%.1f seconds left in recording' % remaining)
-        i += 1
+        chunk_num += 1
 
 
-def run_session(duration, filepath, stream_manager):
+def run_session(duration, sources, filepath, stream_manager):
     info = StreamInfo(
         name='Markers',
         type='Markers',
@@ -176,7 +195,7 @@ def run_session(duration, filepath, stream_manager):
 
     record_process = Process(
         target=record_chunks,
-        args=(duration, filepath, outlet, stream_manager),
+        args=(duration, sources, filepath, stream_manager, outlet),
     )
     record_process.start()
     capture_input(duration, outlet)
