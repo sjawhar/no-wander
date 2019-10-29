47c8199a30d857a9d02ce0e94f164b543373c2f1
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-10-29 14:12:21 -0500

    Input and signal recording in processes managed by session module

--- app/record.py
+++ app/record.py
@@ -4,52 +4,55 @@ from muselsl import list_muses, record
 from pathlib import PurePath
 from psychopy import core, event
 from time import sleep, time
-from .constants import PACKAGE_NAME
+from .constants import (
+    EVENT_RECORD_CHUNK_START,
+    EVENT_SESSION_END,
+    EVENT_USER_RECOVER,
+    PACKAGE_NAME,
+)
 
 
+CHUNK_DURATION_BUFFER = 5
 CHUNK_DURATION_MAX = 300
 CHUNK_DURATION_MIN = 10
 KEYS_QUIT = ['esc', 'q']
-VALUE_END = 2
-VALUE_RESPONSE = 1
 
 logger = logging.getLogger(PACKAGE_NAME + '.' + __name__)
 
 
-def capture_input(duration, outlet, conn):
+def record_input(duration, queue):
     start_time = time()
     clock = core.Clock()
     clock.reset(-start_time)
     end_time = start_time + duration
     time_left = duration
 
-    logger.info(f'Running session for {duration} seconds')
-    logger.debug(f'Session to run from {start_time} to {end_time}')
+    logger.info(f'Recording user input for {duration} seconds')
+    logger.debug(f'Input recording to run from {start_time} to {end_time}')
     while time_left > 0:
         keys = event.waitKeys(
             maxWait=time_left,
             timeStamped=clock,
         )
         if keys is None:
-            logger.debug('Session time expired')
+            logger.debug('User input recording time expired')
             break
 
         key, timestamp = keys[0]
         logger.debug(f'{key} pressed at time {timestamp}')
-        value = VALUE_END if key in KEYS_QUIT else VALUE_RESPONSE
-        outlet.push_sample([value], timestamp)
-
-        if value == VALUE_END:
-            logger.info(f'{key} pressed, ending session')
-            conn.send(VALUE_END)
+        if key in KEYS_QUIT:
+            logger.info(f'{key} pressed, ending user input recording')
+            queue.put([EVENT_SESSION_END, timestamp])
             break
+
+        queue.put([EVENT_USER_RECOVER, timestamp])
         time_left = end_time - clock.getTime()
 
-    conn.close()
-    logger.info('Session ended')
+    queue.close()
+    logger.info('User input recording ended')
 
 
-def record_chunks(duration, sources, filepath, stream_manager, conn):
+def record_signals(duration, sources, filepath, stream_manager, conn):
     filename_parts = filepath.name.split('.')
     filename_parts = filename_parts[:-1] + [''] * 2 + filename_parts[-1:]
     start_time = time()
@@ -58,6 +61,10 @@ def record_chunks(duration, sources, filepath, stream_manager, conn):
     chunk_num = 1
     remaining = duration
     while remaining > CHUNK_DURATION_MIN:
+        if conn.poll() and conn.recv() == [EVENT_SESSION_END]:
+            logger.info('Session end signal received. Ending...')
+            break
+
         chunk_duration = min(remaining, CHUNK_DURATION_MAX)
         filename_parts[-2] = str(chunk_num)
         record_processes = []
@@ -77,30 +84,31 @@ def record_chunks(duration, sources, filepath, stream_manager, conn):
             process.start()
             record_processes.append(process)
 
-        # # TODO: Push dummy sample to preserve recording if no responses
-        # record_processes[0].join(5)
-        # dummy_timestamp = time()
-        # logger.debug(f'Pushing dummy sample at {dummy_timestamp}')
-        # conn.send([VALUE_START, dummy_timestamp])
+        record_processes[0].join(CHUNK_DURATION_BUFFER)
+        if not record_processes[0].is_alive():
+            logger.warning('Error in stream. Restarting...')
+            (process.terminate() for process in record_processes)
+            stream_manager = stream_manager()
+            logger.info('Stream restarted')
+            remaining = get_remaining()
+            continue
 
-        record_processes[0].join(chunk_duration + 10)
+        dummy_timestamp = time()
+        logger.debug(f'Signaling chunk start at {dummy_timestamp}')
+        conn.send([EVENT_RECORD_CHUNK_START, dummy_timestamp])
+
+        record_processes[0].join(chunk_duration + CHUNK_DURATION_BUFFER)
+        # Give other recording process a chance to end normally
         sleep(2)
         for si in range(len(sources)):
             if record_processes[si].is_alive():
                 logger.warning(f'{sources[si]} recording process has idled. Terminating...')
                 record_processes[si].terminate()
 
-        chunk_time = remaining - get_remaining()
+        new_remaining = get_remaining()
+        chunk_time = remaining - new_remaining
         logger.debug(f'Chunk {chunk_num} took {chunk_time} seconds')
-        if conn.recv() == VALUE_END:
-            logger.info('Session end signal received. Ending...')
-            break
-        elif chunk_time < chunk_duration:
-            logger.warning('Error in stream. Restarting...')
-            stream_manager = stream_manager()
-            logger.info('Stream restarted')
-
-        remaining = get_remaining()
+        remaining = new_remaining
         logger.debug('%.1f seconds left in recording' % remaining)
         chunk_num += 1
 
--- app/session.py
+++ app/session.py
@@ -1,12 +1,20 @@
 import logging
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Pipe, Queue
 from pathlib import Path
 from psychopy import core, sound, visual
 from pylsl import StreamInfo, StreamOutlet, IRREGULAR_RATE
-from .constants import PACKAGE_NAME
-from .record import capture_input, record_chunks
+from .record import record_input, record_signals
+from .constants import (
+    EVENT_RECORD_CHUNK_START,
+    EVENT_SESSION_END,
+    EVENT_USER_RECOVER,
+    PACKAGE_NAME,
+)
 
 
+MARKER_DUMMY = 0
+MARKER_END = 2
+MARKER_RECOVER = 1
 SOUND_BELL = (Path(__file__).parent / 'assets' / 'bell.wav').resolve()
 
 logger = logging.getLogger(PACKAGE_NAME + '.' + __name__)
@@ -32,6 +40,25 @@ def play_bell():
         logger.error('Could not play bell sound')
 
 
+def handle_signals_event(event, outlet):
+    logger.debug(f'Received {event} from signal recording')
+    if event[0] == EVENT_RECORD_CHUNK_START:
+        logger.debug(f'Pushing dummy sample at {event[1]}')
+        outlet.push_sample([MARKER_DUMMY], event[1])
+
+
+def handle_input_event(event, outlet):
+    logger.debug(f'Received {event} from input recording')
+    if event[0] == EVENT_SESSION_END:
+        outlet.push_sample([MARKER_END], event[1])
+        return False
+
+    if event[0] == EVENT_USER_RECOVER:
+        outlet.push_sample([MARKER_RECOVER], event[1])
+
+    return True
+
+
 def run_session(duration, sources, filepath, stream_manager):
     info = StreamInfo(
         name='Markers',
@@ -53,16 +80,40 @@ def run_session(duration, sources, filepath, stream_manager):
     session_window.flip()
     play_bell()
 
-    record_conn, input_conn = Pipe()
-    record_process = Process(
-        target=record_chunks,
-        args=(duration, sources, filepath, stream_manager, record_conn),
+    signals_conn, conn = Pipe()
+    signals_process = Process(
+        target=record_signals,
+        args=(duration, sources, filepath, stream_manager, conn),
     )
-    record_process.start()
-    capture_input(duration, outlet, input_conn)
+    signals_process.start()
 
+    input_queue = Queue()
+    input_process = Process(
+        target=record_input,
+        args=(duration, input_queue),
+    )
+    input_process.daemon = True
+    input_process.start()
+
+    while signals_process.is_alive() or input_process.is_alive():
+        try:
+            if signals_conn.poll():
+                handle_signals_event(signals_conn.recv(), outlet)
+            if not input_queue.empty():
+                if handle_input_event(input_queue.get_nowait(), outlet):
+                    continue
+                logger.info(f'Triggering end of session at {event[1]}')
+                signals_conn.send([EVENT_SESSION_END])
+                signals_process.join()
+                logger.info(f'Session ended at {event[1]}')
+                break
+        except KeyboardInterrupt:
+            break
+
+    signals_conn.close()
     session_window.close()
     play_bell()
 
-    record_process.join()
+    if signals_process.is_alive():
+        signals_process.terminate()
     core.quit()
